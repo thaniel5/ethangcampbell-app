66ca447d5842b8134bde2965d6918fea75f98fc8
Ethan Campbell <ethangcampbell@gmail.com>
2023-07-22 17:32:06 -0400

    added document wrapper to root

--- app/root.tsx
+++ app/root.tsx
@@ -8,7 +8,10 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  isRouteErrorResponse,
+  useRouteError,
 } from "@remix-run/react";
+import { type PropsWithChildren } from "react";
 
 import stylesheet from "~/tailwind.css";
 
@@ -17,7 +20,7 @@ export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
-export default function App() {
+function Document({ children }: PropsWithChildren) {
   return (
     <html lang="en" className="h-full">
       <head>
@@ -41,13 +44,53 @@ export default function App() {
             </NavLink>
           </div>
         </header>
-        <main className="text-center">
-          <Outlet />
-        </main>
+        <main className="text-center">{children}</main>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
+  );
+}
+
+export default function App() {
+  return (
+    <Document>
+      <Outlet />
+    </Document>
+  );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  let content = null;
+
+  if (isRouteErrorResponse(error)) {
+    content = (
+      <div>
+        <h1>
+          {error.status} {error.statusText}
+        </h1>
+        <p>{error.data}</p>
+      </div>
+    );
+  } else if (error instanceof Error) {
+    content = (
+      <div>
+        <h1>Error</h1>
+        <p>{error.message}</p>
+        <p>The stack trace is:</p>
+        <pre>{error.stack}</pre>
+      </div>
+    );
+  } else {
+    content = <h1>Unknown Error</h1>;
+  }
+
+  return (
+    <Document>
+      <div className="py-40 text-gray-200">{content}</div>
+    </Document>
   );
 }
--- remix.config.js
+++ remix.config.js
@@ -3,10 +3,10 @@ module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   future: {
     v2_errorBoundary: true,
+    v2_headers: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
     v2_routeConvention: true,
-    v2_headers: true,
   },
   ignoredRouteFiles: ["**/.*", "**/*.test.{js,jsx,ts,tsx}"],
   postcss: true,
